dc53246010f1a7facd3398cf68a5a24650ea7b1d
Tatyana-Essentya <83660115+Likeout@users.noreply.github.com>
2022-01-29 17:53:45 +0300

    kl

---
+++ PS223020Server-main/PS223020Server/PS223020Server.DataAccess.Core/Test/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS223020Server.DataAccess.Core.Test
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public string Brand { get; set; }
+        public User Users { get; set; }
+    }
+}
---
+++ PS223020Server-main/PS223020Server/PS223020Server.DataAccess.Core/Test/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS223020Server.DataAccess.Core.Test
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string SecondName { get; set; }
+        List<Car>Cars { get; set; }
+    }
+}
