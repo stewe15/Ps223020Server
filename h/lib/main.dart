ce614871f511ee374383110fdaf723c7439f6635
Tatyana-Essentya <83660115+Likeout@users.noreply.github.com>
2022-01-29 16:55:26 +0300

    xcv

---
+++ h/lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void:main(){
+  rupApp(daas());
+}
+ class daas extends StatelessWidget {
+   const daas({Key key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home:daas,
+     );
+   }
+ }
+
+
+class das extends StatefulWidget {
+  das({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    
+    body: SafeArea(
+      child:Column(children: [
+        Container(
+          width: 75.0,
+          height: 75.0,
+          child:Image.asset("resourses/158714654911506114.jpg"),
+          column ()
+
+          )
+        width: size.width*0,8
+        height:150.0
+        Decoration:BoxDecoration(
+          BorderRadius:BorderRadius.all(Radius.circular(20.0))
+          color: Colors.Colors.deepOrangeAccent
+          backgroundColor: Color(0xFFD3D3D3),
+        )
+        ],
+      ) ,
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+лёша лох
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ха-ха-ах
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ультрамеганасрал
